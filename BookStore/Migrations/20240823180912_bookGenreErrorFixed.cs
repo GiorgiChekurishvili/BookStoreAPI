a819003116a05ae811e5be8eb67fbbdf539f62cf
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-08-24 00:22:14 +0400

    added new migrations

---
+++ BookStore/Migrations/20240823180912_bookGenreErrorFixed.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class bookGenreErrorFixed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookGenre_Books_BookId",
+                table: "BookGenre");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookGenre_Genres_GenreId",
+                table: "BookGenre");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_BookGenre",
+                table: "BookGenre");
+
+            migrationBuilder.RenameTable(
+                name: "BookGenre",
+                newName: "BookGenres");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BookGenre_GenreId",
+                table: "BookGenres",
+                newName: "IX_BookGenres_GenreId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BookGenre_BookId",
+                table: "BookGenres",
+                newName: "IX_BookGenres_BookId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_BookGenres",
+                table: "BookGenres",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookGenres_Books_BookId",
+                table: "BookGenres",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookGenres_Genres_GenreId",
+                table: "BookGenres",
+                column: "GenreId",
+                principalTable: "Genres",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookGenres_Books_BookId",
+                table: "BookGenres");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookGenres_Genres_GenreId",
+                table: "BookGenres");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_BookGenres",
+                table: "BookGenres");
+
+            migrationBuilder.RenameTable(
+                name: "BookGenres",
+                newName: "BookGenre");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BookGenres_GenreId",
+                table: "BookGenre",
+                newName: "IX_BookGenre_GenreId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BookGenres_BookId",
+                table: "BookGenre",
+                newName: "IX_BookGenre_BookId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_BookGenre",
+                table: "BookGenre",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookGenre_Books_BookId",
+                table: "BookGenre",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookGenre_Genres_GenreId",
+                table: "BookGenre",
+                column: "GenreId",
+                principalTable: "Genres",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
