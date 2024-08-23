de696310d86a2286794b28a1ec5a32237f963a0e
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-08-24 00:24:53 +0400

    added new genrerepository

---
+++ BookStore/Repositories/GenreRepository.cs
@@ -0,0 +1,39 @@
+﻿using BookStore.Data;
+using BookStore.Entities;
+
+namespace BookStore.Repositories
+{
+    public class GenreRepository : IGenreRepository
+    {
+        private readonly BookStoreContext _context;
+        public GenreRepository(BookStoreContext context)
+        {
+            _context = context;
+        }
+
+        public Task DeleteGenre(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Genre> GetGenreById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Genre> GetGenres()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Genre> InputGenres(Genre genre)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateGenres(Genre genre)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BookStore/Repositories/IGenreRepository.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.DTOs;
+using BookStore.Entities;
+
+namespace BookStore.Repositories
+{
+    public interface IGenreRepository
+    {
+        Task<Genre> GetGenres();
+        Task<Genre> GetGenreById(int id);
+        Task<Genre> InputGenres(Genre genre);
+        Task UpdateGenres(Genre genre);
+        Task DeleteGenre(int id);
+
+
+    }
+}
