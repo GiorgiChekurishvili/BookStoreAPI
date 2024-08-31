9fc8030e4b498715763e7def2f9cc9e4ed7797ba
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-09-01 00:15:54 +0400

    added dependancy injection scoped for author service

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -4,6 +4,7 @@ using BookStore.Repositories.BookGenresRepository;
 using BookStore.Repositories.BookRepository;
 using BookStore.Repositories.GenreRepository;
 using BookStore.Repositories.PublisherRepository;
+using BookStore.Services.AuthorService;
 using BookStore.Services.BookService;
 using BookStore.Services.GenreService;
 
@@ -26,6 +27,7 @@ builder.Services.AddScoped<IPublisherRepository , PublisherRepository>();
 
 builder.Services.AddScoped<IBookService, BookService>();
 builder.Services.AddScoped<IGenreService, GenreService>();
+builder.Services.AddScoped<IAuthorService, AuthorService>();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
