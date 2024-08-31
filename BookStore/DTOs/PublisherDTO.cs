fd0da55ce954fb1d9169c09936b5521cb9736b09
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-09-01 00:41:17 +0400

    publisherDTO modified

--- BookStore/DTOs/PublisherDTO.cs
+++ BookStore/DTOs/PublisherDTO.cs
@@ -9,5 +9,6 @@ namespace BookStore.DTOs
         public string? Address { get; set; }
         public string? Website { get; set; }
         public string? PhoneNumber { get; set; }
+        public string? Nationality { get; set; }
     }
 }
