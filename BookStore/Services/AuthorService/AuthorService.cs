a4f7602dcdadfe243c2f7c15146a0d0ceffda149
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-09-01 00:15:19 +0400

    new author service added

---
+++ BookStore/Services/AuthorService/AuthorService.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using BookStore.DTOs;
+using BookStore.Entities;
+using BookStore.Repositories.AuthorRepository;
+
+namespace BookStore.Services.AuthorService
+{
+    public class AuthorService : IAuthorService
+    {
+        readonly IMapper _mapper;
+        readonly IAuthorRepository _authorRepository;
+        public AuthorService(IAuthorRepository authorRepository, IMapper mapper)
+        {
+            _authorRepository = authorRepository;
+            _mapper = mapper;
+        }
+        public async Task DeleteAuthorService(int id)
+        {
+            await _authorRepository.DeleteAuthor(id);
+        }
+
+        public async Task UpdateAuthor(int id, AuthorDTO author)
+        {
+            var map = _mapper.Map<Author>(author);
+            map.Id = id;
+            await _authorRepository.UpdateAuthor(map);
+        }
+
+        public async Task UploadAuthor(AuthorDTO author)
+        {
+            var map = _mapper.Map<Author>(author);
+            await _authorRepository.CreateAuthor(map);
+
+        }
+
+        public async Task<IEnumerable<AuthorRetrieveDTO>> ViewAllAuthors()
+        {
+            var data = await _authorRepository.RetrieveAllAuthors();
+            var map = _mapper.Map<IEnumerable<AuthorRetrieveDTO>>(data);
+            return map;
+        }
+
+        public async Task<AuthorRetrieveDTO> ViewAuthorById(int id)
+        {
+            var data = await _authorRepository.RetrieveAuthorById(id);
+            var map = _mapper.Map<AuthorRetrieveDTO>(data);
+            return map;
+        }
+    }
+}
---
+++ BookStore/Services/AuthorService/IAuthorService.cs
@@ -0,0 +1,13 @@
+﻿using BookStore.DTOs;
+
+namespace BookStore.Services.AuthorService
+{
+    public interface IAuthorService
+    {
+        Task<IEnumerable<AuthorRetrieveDTO>> ViewAllAuthors();
+        Task<AuthorRetrieveDTO> ViewAuthorById(int id);
+        Task UploadAuthor(AuthorDTO author);
+        Task UpdateAuthor(int id, AuthorDTO author);
+        Task DeleteAuthorService(int id);
+    }
+}
