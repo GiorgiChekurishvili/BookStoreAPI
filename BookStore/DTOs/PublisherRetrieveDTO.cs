65a7924eb8cd86ba653794b6cd4db81c0ad3fc5a
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-09-01 00:41:31 +0400

    new dto added

---
+++ BookStore/DTOs/PublisherRetrieveDTO.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.Entities;
+
+namespace BookStore.DTOs
+{
+    public class PublisherRetrieveDTO
+    {
+        public int Id { get; set; }
+        public required string PublisherName { get; set; }
+        public string? Address { get; set; }
+        public string? Website { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Nationality { get; set; }
+        public List<string>? BooksPublished { get; set; }
+    }
+}
