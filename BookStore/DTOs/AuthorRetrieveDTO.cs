2acee282de23a44284ac20e063286c8bf1eb1210
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-09-01 00:15:01 +0400

    new dto added for get requests

---
+++ BookStore/DTOs/AuthorRetrieveDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.DTOs
+{
+    public class AuthorRetrieveDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        public string AuthorName { get; set; } = string.Empty;
+        [Required]
+        public string Biography { get; set; } = string.Empty;
+        public required List<string> BooksWritten { get; set; }
+        public string? DateOfBirth { get; set; }
+        public string? DateOfDeath { get; set; }
+    }
+}
