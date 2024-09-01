666fd1852d71189194dce0d72703fd5b2f20ab06
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-09-02 01:06:25 +0400

    new services added for authrepository

---
+++ BookStore/Services/AuthService/AuthService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using BookStore.DTOs;
+using BookStore.Entities;
+using BookStore.Repositories.AuthRepository;
+using System.Security.Cryptography;
+
+namespace BookStore.Services.AuthService
+{
+    public class AuthService : IAuthService
+    {
+        readonly IMapper _mapper;
+        readonly IAuthRepository _authRepository;
+        public AuthService(IMapper mapper, IAuthRepository authRepository)
+        {
+            _authRepository = authRepository;
+            _mapper = mapper;
+        }
+        public async Task<string?> Login(UserLoginDTO loginDTO)
+        {
+            var data = await _authRepository.UserLogin(loginDTO.Password, loginDTO.Email);
+            if (data != null)
+            {
+                return data;
+            }
+            return null;
+        }
+
+        public async Task<UserRegisterDTO?> Register(UserRegisterDTO registerDTO)
+        {
+            CreatePasswordHash(registerDTO.Password, out byte[] passwordHash, out byte[] passwordSalt);
+            var map = _mapper.Map<User>(registerDTO);
+            map.PasswordHash = passwordHash;
+            map.PasswordSalt = passwordSalt;
+            var user = await _authRepository.UserRegistration(map);
+            if (user == null)
+            {
+                return null; 
+            }
+            return registerDTO;
+            
+        }
+        public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = System.Text.Encoding.UTF8.GetBytes(password);
+            }
+        }
+        
+    }
+}
---
+++ BookStore/Services/AuthService/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using BookStore.DTOs;
+
+namespace BookStore.Services.AuthService
+{
+    public interface IAuthService
+    {
+        Task<string?> Login(UserLoginDTO loginDTO);
+        Task<UserRegisterDTO?> Register(UserRegisterDTO registerDTO);
+    }
+}
