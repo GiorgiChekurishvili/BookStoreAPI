655a4fbf6d3a83cfa5bbaa7346de4bb6d5d620a5
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-09-02 01:07:12 +0400

    added scoped for authService and for authRepository

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -1,10 +1,12 @@
 using BookStore.Data;
 using BookStore.Repositories.AuthorRepository;
+using BookStore.Repositories.AuthRepository;
 using BookStore.Repositories.BookGenresRepository;
 using BookStore.Repositories.BookRepository;
 using BookStore.Repositories.GenreRepository;
 using BookStore.Repositories.PublisherRepository;
 using BookStore.Services.AuthorService;
+using BookStore.Services.AuthService;
 using BookStore.Services.BookService;
 using BookStore.Services.GenreService;
 using BookStore.Services.PublisherService;
@@ -25,11 +27,13 @@ builder.Services.AddScoped<IBookGenresRepository, BookGenresRepository>();
 builder.Services.AddScoped<IGenreRepository, GenreRepository>();
 builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
 builder.Services.AddScoped<IPublisherRepository , PublisherRepository>();
+builder.Services.AddScoped<IAuthRepository, AuthRepository>();
 
 builder.Services.AddScoped<IBookService, BookService>();
 builder.Services.AddScoped<IGenreService, GenreService>();
 builder.Services.AddScoped<IAuthorService, AuthorService>();
 builder.Services.AddScoped<IPublisherService, PublisherService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
