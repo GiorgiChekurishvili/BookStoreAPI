0f4f6e6af1ff0a7dd047fdce67af6616f151ff09
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-09-02 01:05:28 +0400

    new authentication repository added

---
+++ BookStore/Repositories/AuthRepository/AuthRepository.cs
@@ -0,0 +1,52 @@
+﻿using BookStore.Data;
+using BookStore.Entities;
+using System.Security.Cryptography;
+
+namespace BookStore.Repositories.AuthRepository
+{
+    public class AuthRepository : IAuthRepository
+    {
+        readonly BookStoreContext _context;
+        public AuthRepository(BookStoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<string?> UserLogin(string password, string email)
+        {
+            var data = await _context.Users.Where(x=>x.Email == email).FirstOrDefaultAsync();
+            if (data == null)
+            {
+                return null;
+            }
+            if (VerifyPasswordHash(password, data.PasswordHash!, data.PasswordSalt!))
+            {
+                return "token";
+            }
+            return null;
+            
+
+        }
+
+        public async Task<User?> UserRegistration(User user)
+        {
+            var ifexists = await _context.Users.Where(x=>x.Email == user.Email).FirstOrDefaultAsync();
+            if (ifexists != null)
+            {
+                return null;
+            }
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedhash = System.Text.Encoding.UTF8.GetBytes(password);
+                return computedhash.SequenceEqual(passwordHash);
+            }
+        }
+    }
+}
---
+++ BookStore/Repositories/AuthRepository/IAuthRepository.cs
@@ -0,0 +1,11 @@
+﻿using BookStore.Entities;
+
+namespace BookStore.Repositories.AuthRepository
+{
+    public interface IAuthRepository
+    {
+        Task<string?> UserLogin(string password, string email);
+        Task<User?> UserRegistration(User user);
+
+    }
+}
