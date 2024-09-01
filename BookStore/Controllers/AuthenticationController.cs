33658df7b5060eab266083bfadd451ae0139d3a4
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-09-02 01:07:31 +0400

    added new controller for requests and responses

---
+++ BookStore/Controllers/AuthenticationController.cs
@@ -0,0 +1,39 @@
+﻿using BookStore.DTOs;
+using BookStore.Services.AuthService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        readonly IAuthService _authService;
+        public AuthenticationController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserRegisterDTO>> Register(UserRegisterDTO userRegisterDTO)
+        {
+            var data = await _authService.Register(userRegisterDTO);
+            if (data == null)
+            {
+                return BadRequest("This email is already registered");
+            }
+            return Ok();
+        }
+        [HttpPost("login")]
+        public async Task<ActionResult<UserLoginDTO>> Login(UserLoginDTO userLoginDTO)
+        {
+            var data = await _authService.Login(userLoginDTO);
+            if (data == null)
+            {
+                return BadRequest();
+            }
+            return Ok(data);
+        }
+    }
+}
