bf048f0ae6d04d53da7220d3c1d7080b5e8a4d03
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-09-03 01:00:07 +0400

    added new entity

---
+++ BookStore/Entities/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using Azure.Core.Pipeline;
+
+namespace BookStore.Entities
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User? user { get; set; }
+        public int BookId { get; set; }
+        public Book? book { get; set; }
+        public int BoughtQuantity { get; set; }
+        public DateTime BoughtDatetime { get; set; }
+    }
+}
