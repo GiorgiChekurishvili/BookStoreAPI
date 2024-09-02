5859a1945c29c226b3ea0534137fcc31380f6690
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-09-03 01:03:56 +0400

    added StockLeft property to DTO

--- BookStore/DTOs/Book/BookRetrieveDTO.cs
+++ BookStore/DTOs/Book/BookRetrieveDTO.cs
@@ -14,6 +14,7 @@ namespace BookStore.DTOs.Book
         public required string AgeRecommend { get; set; }
         public required string PublisherName { get; set; }
         public int StockQuantity { get; set; }
+        public int StockLeft {  get; set; }
         public DateTime ReleaseDate { get; set; }
         public DateTime AddedDate { get; set; }
 
