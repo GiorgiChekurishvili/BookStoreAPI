5c3c19e40a05dee36f7d7b846fe034b82fe611fa
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-09-02 19:09:58 +0400

    changed registerDTO for email address

--- BookStore/DTOs/UserRegisterDTO.cs
+++ BookStore/DTOs/UserRegisterDTO.cs
@@ -1,9 +1,12 @@
-﻿namespace BookStore.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.DTOs
 {
     public class UserRegisterDTO
     {
         public required string Name { get; set; }
         public required string Surname { get; set; }
+        [EmailAddress]
         public required string Email { get; set; }
         public required string Password { get; set; }
     }
