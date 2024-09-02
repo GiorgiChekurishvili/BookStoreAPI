0285e200ffcaf58143a0894a5de229192ab2060f
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2024-09-03 01:04:15 +0400

    added new DTOs for Transaction Entities

---
+++ BookStore/DTOs/Transaction/BuyBookTransactionDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStore.DTOs.Transaction
+{
+    public class BuyBookTransactionDTO
+    {
+        public int BookId { get; set; }
+        public int UserId { get; set; }
+        public int BoughtQuantity { get; set; }
+    }
+}
---
+++ BookStore/DTOs/Transaction/TransactionDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStore.DTOs.Transaction
+{
+    public class TransactionDTO
+    {
+        public int BookId { get; set; }
+        public required string BookName { get; set; }
+        public int BoughtQuantity { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime BoughtDatetime { get; set; }
+    }
+}
